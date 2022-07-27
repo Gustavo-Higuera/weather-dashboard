15441f0953d8d7083008948560a091b07bec346b
Coding Padawan <gustavohiguera9715@gmail.com>
2022-07-26 18:09:16 -0700

    completed fetch requests

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,8 @@
-var cityInput = $("#search-input").val();
-cityInput = "Phoenix";
-
+var searchBtn = $("#search-btn");
 var apiKey = "d4f0a867ce4cfa7304c028826c6a3551";
 
-function getCity(){
-  var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${apiKey}`
+function getCity(city){
+  var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
 
   fetch(geocodeUrl)
     .then(function (response) {
@@ -18,8 +16,6 @@ function getCity(){
       getWeather(data[0].lat, data[0].lon);
     });
   }
-
-
 
     function getWeather(lat, lon){
       var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
@@ -35,6 +31,10 @@ function getCity(){
       });
     }
 
-    getCity();
-    getWeather();
+    searchBtn.on("click", function(){
+      var cityInput = $(this).siblings("#search-input").val();
+
+      getCity(cityInput);
+    });
+
 
